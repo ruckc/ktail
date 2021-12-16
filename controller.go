85ce4e31c699017137bf97b6b77c4de8a3dce85e
Curtis Ruck <curtis@ruck.io>
2021-12-16 14:18:00 -0500

    simplfiy buildKey when containerID doesn't exist so multiple tailers aren't created

--- controller.go
+++ controller.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 	"sync"
 	"time"
 
+	"github.com/fatih/color"
 	"k8s.io/client-go/kubernetes"
 
 	v1 "k8s.io/api/core/v1"
@@ -196,10 +198,12 @@ func (ctl *Controller) shouldIncludeContainer(pod *v1.Pod, container *v1.Contain
 }
 
 func (ctl *Controller) addContainer(pod *v1.Pod, container *v1.Container, initialAdd bool) {
+	yellow := color.New(color.FgYellow)
 	ctl.Lock()
 	defer ctl.Unlock()
 
 	key := buildKey(pod, container)
+	yellow.Fprintf(os.Stderr, "addContainer: %s\n", key)
 	if _, ok := ctl.tailers[key]; ok {
 		return
 	}
@@ -270,7 +274,7 @@ func (ctl *Controller) getStartTimestamp(
 }
 
 func buildKey(pod *v1.Pod, container *v1.Container) string {
-	return fmt.Sprintf("%s/%s/%s", pod.Namespace, pod.Name, findContainerID(pod, container))
+	return fmt.Sprintf("%s/%s", pod.Namespace, pod.Name)
 }
 
 func findContainerID(pod *v1.Pod, container *v1.Container) string {
